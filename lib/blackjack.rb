09be015979d423c87bf5cfcf30e8d6cdd3615e80
Bashir <bashiralhanshali@yahoo.com>
2020-04-30 14:29:36 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -68,7 +68,7 @@ def runner
   welcome()
   card_total = initial_round() 
   until card_total > 21 do 
-    card_total += hit?(card_total)
+    card_total = hit?(card_total)
   end 
   end_game
 end
